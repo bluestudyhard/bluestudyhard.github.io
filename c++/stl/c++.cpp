a7083cf571aaf1f13e77179400d17e8d8f54be20
blue <3164752596@qq.com>
2022-03-01 15:40:44 +0800

    1

--- c++/stl/c++.cpp
+++ c++/stl/c++.cpp
@@ -1,39 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+int a[500050], b[50050], c[50050] = {0};
+int len = 1, i, j;
+void f(int n)
+{
+    a[1] = 1;
+    b[1] = 2;
+    for (i = 3; i <= n; i++)
+    {
+        for (j = 1; j <= len; j++)
+            c[j] = a[j] + b[j];
+        for (j = 1; j <= len; j++)
+        {
+            if (c[j] > 9)
+            {
+                c[j + 1] += c[j] / 10;
+                c[j] %= 10;
+                if (j + 1 > len)
+                    len++;
+            }
+        }
+        for (j = 1; j <= len; j++)
+        {
+            a[j] = b[j];
+            b[j] = c[j];
+        }
+    }
+}
 int main()
 {
-    list<int> k;
-    int n, i, m;
-    string a;
-    scanf("%d", &n);
-    while (n--)
+    int n;
+    cin >> n;
+    if (n < 3)
     {
-        cin >> a;
-        if (a == "insert")
-        {
-            cin >> m;
-            k.push_front(m);
-        }
-        else if (a == "deleteFirst")
-            k.pop_front();
-        else if (a == "deleteLast")
-            k.pop_back();
-        else if (a == "delete")
-        {
-            cin >> m;
-            for (auto i = k.begin(); i != k.end(); i++)
-                if (*i == m)
-                {
-                    k.erase(i);
-                    break;
-                }
-        }
+        cout << n;
+        return 0;
     }
-    int it = 0;
-    for (auto i : k)
-    {   if(it++)
-        cout <<" ";
-        cout << i;
-    }
-    cout<<endl;
+    f(n);
+    for (i = len; i >= 1; i--)
+        cout << c[i];
 }
