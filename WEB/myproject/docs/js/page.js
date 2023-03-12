2bb2c246ec0aec4d029704cd89bdc09be4131763
bluestudyhard <98652207+bluestudyhard@users.noreply.github.com>
2023-03-13 00:18:44 +0800

    2023.3.12

--- WEB/myproject/docs/js/page.js
+++ WEB/myproject/docs/js/page.js
@@ -3,18 +3,17 @@
 let ser = document.querySelector(".serachbody");
 let form = document.querySelector("form");
 const body = document.querySelector("body");
-
+let count = Math.floor(Math.random() * 14) + 1;
 function randomPage() {
-  let count = Math.floor(Math.random() * 10) + 1;
-  body.style.backgroundImage = `url(page/page${count}.png)`;
+  body.style.backgroundImage = `url(page/page${count}.webp)`;
 }
 randomPage();
 let date = new Date();
 let hours = date.getHours();
 if (hours >= 23 || hours <= 6) {
-  body.style.backgroundImage = `url(page/page10.png)`;
+  body.style.backgroundImage = `url(page/page${count}.webp)`;
 } else if (hours >= 6 && hours <= 12) {
-  body.style.backgroundImage = `url(page/page9.png)`;
+  body.style.backgroundImage = `url(page/page${count}.webp)`;
 }
 console.log(hours);
 ser.onfocus = () => {
@@ -38,13 +37,14 @@ const btnright = document.querySelector(".button-right");
 function changeImage() {
   let i = 0;
   btnright.onclick = () => {
-    if (i == 10 || i > 10) i = 0;
-    body.style.backgroundImage = `url(page/page${i}.png)`;
+    if (i == 14 || i > 14) i = 0;
+    body.style.backgroundImage = `url(page/page${i}.webp)`;
     i++;
+    console.log(i);
   };
   btnleft.onclick = () => {
-    if (i < 0) i = 10;
-    body.style.backgroundImage = `url(page/page${i}.png)`;
+    if (i < 0) i = 14;
+    body.style.backgroundImage = `url(page/page${i}.webp)`;
     i--;
   };
 }
