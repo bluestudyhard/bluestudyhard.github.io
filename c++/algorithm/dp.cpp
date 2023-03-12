92410c5cf080f4dc9fa260d587cfd03a86480055
bluestudyhard <98652207+bluestudyhard@users.noreply.github.com>
2023-03-12 23:01:42 +0800

    111

---
+++ c++/algorithm/dp.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a[100];
+int fib(int n)
+{
+    a[0] = 1;
+    a[1] = 1;
+    if (n < 2)
+    {
+
+        return a[n];
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        a[i] = a[i - 1] + a[i - 2];
+    }
+    return a[n];
+}
+/*经典的最优子结构问题*/
+/*据钢条*/
+/*递归版*/
+int cut(int a[], int n)
+{
+    if (n == 0)
+        return 0;
+    int sum = -1;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = max(sum, a[i - 1] + cut(a, n - i)); // p3 = 1+1+1 1+5 8
+        // cut 的递归部分，得到的是所有情况的，sum的值，因为递归嘛，有一个压栈和回溯。
+    }
+    return sum;
+}
+int main()
+{
+    int a[5] = {1, 5, 8, 9};
+    cout << cut(a, 4);
+}
--- c++/algorithm/testtestl.cpp
+++ c++/algorithm/testtestl.cpp
@@ -1,53 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
+// 将一个数拆分成n个数的和 dfs经典了
+void split_add(int n, int m, int a[], int k)
+{
+    if (n == 0)
+    {
+        for (int i = 0; i < k; i++)
+            cout << a[i] << " ";
+        cout << endl;
+    }
+    for (int i = m; i > 0; i--)
+    {
+        if (i <= n)
+        {
+            a[k] = i;
+            split_add(n - i, i, a, k + 1);
+        }
+    }
+}
+void spilt(int n,int a[],int k)
+{
+    
+}
 int main()
 {
     /*打印沙漏*/
-    int m, n = 0;
-    char c;
-    cin >> m >> c;
-    // Sn上半部分 a1 = 3,a2 = 5 a(n-1) = 2n+1  sn = n(n+2) Sn = 2s(n-1)+1 = 2n*n-1
-    for (int i = 1; 2 * n * n - 1 < m;)
-    {
-        n++;
-    }
-    n--;
-    int an1 = 2 * n - 1, x = 0, an2 = 1; // 先求最大层的数目
-    while (an1 > 0)
-    {
-        for (int i = 0; i < x; i++)
-        {
-            cout << " ";
-        }
-        x++;
-        for (int i = an1; i > 0; i--)
-        {
-            cout << c;
-        }
-        cout << endl;
-        m -= an1;
-        an1 -= 2;
-    }
-    // 下半部分
-    x--;
-    while (x > 0)
-    {
-        x--;
-        an2 += 2;
-        m -= an2;
-        for (int i = 0; i < x; i++)
-        {
-            cout << " ";
-        }
-        for (int i = 0; i < an2; i++)
-            cout << c;
-        cout << endl;
-    }
-    if (m == 0)
-        return 0;
-    cout << m;
+    // int m, n = 0;
+    // char c;
+    // cin >> m >> c;
+    // // Sn上半部分 a1 = 3,a2 = 5 a(n-1) = 2n+1  sn = n(n+2) Sn = 2s(n-1)+1 = 2n*n-1
+    // for (int i = 1; 2 * n * n - 1 < m;)
+    // {
+    //     n++;
+    // }
+    // n--;
+    // int an1 = 2 * n - 1, x = 0, an2 = 1; // 先求最大层的数目
+    // while (an1 > 0)
+    // {
+    //     for (int i = 0; i < x; i++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     x++;
+    //     for (int i = an1; i > 0; i--)
+    //     {
+    //         cout << c;
+    //     }
+    //     cout << endl;
+    //     m -= an1;
+    //     an1 -= 2;
+    // }
+    // // 下半部分
+    // x--;
+    // while (x > 0)
+    // {
+    //     x--;
+    //     an2 += 2;
+    //     m -= an2;
+    //     for (int i = 0; i < x; i++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int i = 0; i < an2; i++)
+    //         cout << c;
+    //     cout << endl;
+    // }
+    // if (m == 0)
+    //     return 0;
+    // cout << m;
 
     /**/
     // 前面为1 多减
@@ -78,4 +99,7 @@ int main()
     //     }
     // }
     // return 0;
+    /*dp*/
+    int a[5];
+    split_add(4, 4, a, 0);
 }
